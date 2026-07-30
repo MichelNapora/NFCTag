5f2753fdf8a08c0508eaa14ec2d43d66c1561ca7
MichelNapora <michel.napora@student.efpl.be>
2026-07-30 09:36:59 +0200

    changement mdp employée back

--- backend/src/main/java/com/nfctag/features/auth/AuthService.java
+++ backend/src/main/java/com/nfctag/features/auth/AuthService.java
@@ -69,9 +69,12 @@ public class AuthService {
         Employee employee = this.me(authentication);
 
         if (!this.passwordEncoder.matches(change.currentPassword(), employee.getPasswordHash())) {
-            throw new InvalidCredentialsException("Le mot de passe actuel est incorrect");
+            throw new InvalidCredentialsException("This password is invalid");
         }
 
+        if (this.passwordEncoder.matches(change.newPassword(), employee.getPasswordHash())) {
+            throw new InvalidCredentialsException("The new password must be different");
+        }
         employee.setPasswordHash(this.passwordEncoder.encode(change.newPassword()));
         this.employeeRepository.save(employee);
     }
